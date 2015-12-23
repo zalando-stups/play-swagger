d7a14933b80a27efc3abf4ffc4d7134918c6043c
Slava Schmidt <slavaschmidt@gmx.de>
2015-12-23 11:58:23 +0100

    "#51": AstScalaPlayEnricher split into components, one per enrichment step

--- compiler/src/main/scala/de/zalando/apifirst/generators/AstScalaPlayEnricher.scala
+++ compiler/src/main/scala/de/zalando/apifirst/generators/AstScalaPlayEnricher.scala
@@ -21,19 +21,19 @@ object AstScalaPlayEnricher {
 
   def enrich(app: StrictModel): DenotationTable = {
     val denotationTable = new ScalaModelEnricher(app).apply(emptyTable)
-    denotationTable map { case (r, m) => r -> (m map { case (rr, mm) => rr -> LastListElementMarks.set(mm) })}
+    denotationTable map { case (r, m) => r -> (m map { case (rr, mm) => rr -> LastListElementMarks.set(mm) }) }
   }
 }
 
 /**
   * Enriches AST with information needed to generate scala based model
   */
-class ScalaModelEnricher(app: StrictModel) {
+class ScalaModelEnricher(val app: StrictModel) extends TypesStep with ClassesStep with TraitsStep with AliasesStep {
 
   type SingleStep = ((Reference, Type)) => DenotationTable => Denotation
 
-  private val steps: Seq[SingleStep] = Seq(types _, classes _ , traits _, aliases _)
-  private val types = app.typeDefs.toSeq
+  private val steps: Seq[SingleStep] = Seq(types _, classes _, traits _, aliases _)
+  private val modelTypes = app.typeDefs.toSeq
 
   /**
     * Applies all steps to all types to enrich given denotation table
@@ -42,28 +42,51 @@ class ScalaModelEnricher(app: StrictModel) {
     * @return
     */
   private[apifirst] def apply(implicit table: DenotationTable): DenotationTable = {
-    (table /: steps) { (denotation, step) => (denotation /: types) { (dd, refType) =>
+    (table /: steps) { (denotation, step) => (denotation /: modelTypes) { (dd, refType) =>
       dd.updated(refType._1, dd(refType._1) ++ step(refType)(dd))
-    }}
+    }
+    }
   }
+
+}
+
+trait EnrichmentStep {
+  def app: StrictModel
+}
+
+trait TypesStep extends EnrichmentStep {
 
   /**
     * Puts type information into the denotation table
     * @param typeDef  the DenotationTable to enrich
     * @return
     */
-  private def types(typeDef: (Reference, Type))(table: DenotationTable): Denotation = typeDef match {
+  protected def types(typeDef: (Reference, Type))(table: DenotationTable): Denotation = typeDef match {
     case (ref, t) => Map(COMMON -> Map(TYPE_NAME -> typeName(t, ref)))
   }
 
+  private def typeName(t: Type, r: Reference, suffix: String = "") = t match {
+    case TypeRef(ref) => useType(ref, suffix, "")
+    case p: PrimitiveType => useType(t.name, suffix, "")
+    //       case d: TypeDef => useType(t.name, "", "")
+    case _ => useType(r, suffix, "")
+  }
+
+  private def useType(ref: Reference, suffix: String, prefix: String) = {
+    val fullName = ref.qualifiedName(prefix, suffix)
+    fullName._2
+  }
+}
+
+trait ClassesStep extends EnrichmentStep {
   /**
     * Puts class related information into the denotation table
     * @param typeDef  the DenotationTable to enrich
     * @return
     */
-  private def classes(typeDef: (Reference, Type))(table: DenotationTable): Denotation = typeDef match {
+  protected def classes(typeDef: (Reference, Type))(table: DenotationTable): Denotation = typeDef match {
     case (ref, t: TypeDef) if !ref.simple.contains("AllOf") && !ref.simple.contains("OneOf") =>
-      val traitName = app.discriminators.get(ref).map(_ => Map("name" -> useType(ref, "", "")))
+      val traitName = app.discriminators.get(ref).map(_ => Map("name" -> typeNameDenotation(table, ref)))
       Map("classes" -> (typeDefProps(ref, t, t.fields)(table) + ("trait" -> traitName)))
     case (ref, t: Composite) =>
       val fields = dereferenceFields(t).distinct
@@ -71,37 +94,14 @@ class ScalaModelEnricher(app: StrictModel) {
     case _ => AstScalaPlayEnricher.empty
   }
 
-  /**
-    * Puts trait related information into the denotation table
-    * @param typeDef  the DenotationTable to enrich
-    * @return
-    */
-  private def traits(typeDef: (Reference, Type))(table: DenotationTable): Denotation = typeDef match {
-      case (ref, t: TypeDef) if app.discriminators.contains(ref) =>
-        Map("traits" -> typeDefProps(ref, t, t.fields)(table))
-      case _ => AstScalaPlayEnricher.empty
-    }
-
-  /**
-    * Puts type related information into the denotation table
-    * @param typeDef  the DenotationTable to enrich
-    * @return
-    */
-  private def aliases(typeDef: (Reference, Type))(table: DenotationTable): Denotation = typeDef match {
-      case (ref, t: Container) =>
-        Map("aliases" -> aliasProps(ref, t)(table))
-      //      case (k, v: PrimitiveType) => mapForAlias(k, v)
-      case _ => AstScalaPlayEnricher.empty
-    }
-
-  private def typeDefProps(k: Reference, t: Type, fields: Seq[Field])(table: DenotationTable): Map[String, Any] = {
+  protected def typeDefProps(k: Reference, t: Type, fields: Seq[Field])(table: DenotationTable): Map[String, Any] = {
     Map(
       "name" -> typeNameDenotation(table, k),
-//      "creator_method" -> useType(k, "", "create"),
+      //      "creator_method" -> useType(k, "", "create"),
       "fields" -> fields.map { f =>
         Map(
           "name" -> escape(f.name.simple),
-//          "generator" -> generatorNameForType(f.tpe),
+          //          "generator" -> generatorNameForType(f.tpe),
           "type_name" -> typeNameDenotation(table, f.tpe.name)
         )
       }
@@ -118,37 +118,58 @@ class ScalaModelEnricher(app: StrictModel) {
           throw new IllegalStateException(s"Unexpected contents of Composite $r: $other")
       }
     }
+}
+
+trait TraitsStep extends EnrichmentStep {
+
+  /**
+    * Puts trait related information into the denotation table
+    * @param typeDef  the DenotationTable to enrich
+    * @return
+    */
+  protected def traits(typeDef: (Reference, Type))(table: DenotationTable): Denotation = typeDef match {
+    case (ref, t: TypeDef) if app.discriminators.contains(ref) =>
+      Map("traits" -> typeDefProps(ref, t, t.fields)(table))
+    case _ => AstScalaPlayEnricher.empty
+  }
+
+  protected def typeDefProps(k: Reference, t: Type, fields: Seq[Field])(table: DenotationTable): Map[String, Any] // FIXME should be defined only once
+}
+
+trait AliasesStep extends EnrichmentStep {
+  /**
+    * Puts type related information into the denotation table
+    * @param typeDef  the DenotationTable to enrich
+    * @return
+    */
+  protected def aliases(typeDef: (Reference, Type))(table: DenotationTable): Denotation = typeDef match {
+    case (ref, t: Container) =>
+      Map("aliases" -> aliasProps(ref, t)(table))
+    //      case (k, v: PrimitiveType) => mapForAlias(k, v)
+    case _ => AstScalaPlayEnricher.empty
+  }
+
   private def aliasProps(k: Reference, v: Container)(table: DenotationTable): Map[String, Any] = {
     Map(
-      "name" -> table(k)(COMMON)(TYPE_NAME),
-//      "creator_method" -> useType(k, "", "create"),
+      "name" -> typeNameDenotation(table, k),
+      //      "creator_method" -> useType(k, "", "create"),
       "alias" -> v.alias,
-//      "generator" -> typeName(v, k),
-//      "generator_name" -> generatorNameForType(v),
+      //      "generator" -> typeName(v, k),
+      //      "generator_name" -> generatorNameForType(v),
       "underlying_type" -> v.imports.headOption.map { _ => v.nestedTypes.map { t =>
         typeNameDenotation(table, t.name)
-      }.mkString(", ") }
+      }.mkString(", ")
+      }
     )
-  }
-
-  private def typeName(t: Type, r: Reference, suffix: String = "") = t match {
-    case TypeRef(ref) => useType(ref, suffix, "")
-    case p: PrimitiveType => useType(t.name, suffix, "")
-    //       case d: TypeDef => useType(t.name, "", "")
-    case _ => useType(r, suffix, "")
-  }
-
-  private def useType(ref: Reference, suffix: String, prefix: String) = {
-    val fullName = ref.qualifiedName(prefix, suffix)
-    fullName._2
   }
 }
 
 object DenotationNames {
   val COMMON = "common"
   val TYPE_NAME = "type_name"
+
   def typeNameDenotation(table: DenotationTable, r: Reference): String = {
-    table.get(r).map(_(COMMON)(TYPE_NAME).toString).getOrElse(r.simple)
+    table.get(r).map(_ (COMMON)(TYPE_NAME).toString).getOrElse(r.simple)
   }
 }
 
@@ -163,7 +184,7 @@ object LastListElementMarks {
     case (ss, tt: Map[String, Any]) => ss -> set(tt)
     case (ss, l: List[Map[String, Any]]) =>
       val newList: List[Map[String, Any]] =
-        l.zipWithIndex map { case (le:Map[String, Any],i:Int) => le.updated("last", i == l.length-1) }
+        l.zipWithIndex map { case (le: Map[String, Any], i: Int) => le.updated("last", i == l.length - 1) }
       ss -> newList
     case (ss, other) =>
       ss -> other
