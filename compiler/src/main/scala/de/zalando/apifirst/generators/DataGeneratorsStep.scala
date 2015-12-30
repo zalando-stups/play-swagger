163a76fbd776081c7b1b31238662d9e3b438d6c6
Slava Schmidt <slavaschmidt@gmx.de>
2015-12-30 18:12:45 +0100

    #51: Optimize data generation code

--- compiler/src/main/scala/de/zalando/apifirst/generators/DataGeneratorsStep.scala
+++ compiler/src/main/scala/de/zalando/apifirst/generators/DataGeneratorsStep.scala
@@ -4,9 +4,10 @@ import de.zalando.apifirst.Domain._
 import de.zalando.apifirst.ScalaName._
 import de.zalando.apifirst.naming.Reference
 import DenotationNames._
+
 /**
-  * @author  slasch 
-  * @since   28.12.2015.
+  * @author slasch
+  * @since 28.12.2015.
   */
 
 trait DataGeneratorsStep extends EnrichmentStep[Type] {
@@ -17,6 +18,7 @@ trait DataGeneratorsStep extends EnrichmentStep[Type] {
 
   /**
     * Puts data generators related information into the denotation table
+    *
     * @return
     */
   protected val dataGenerators: SingleStep = typeDef => table =>
@@ -27,10 +29,10 @@ trait DataGeneratorsStep extends EnrichmentStep[Type] {
       Map("test_data_aliases" -> containerGenerator(ref, t)(table))
     case t: PrimitiveType =>
       Map("test_data_aliases" -> containerGenerator(ref, t)(table))
-    case t: TypeDef if ! ref.simple.startsWith("AllOf") =>
+    case t: TypeDef if !ref.simple.startsWith("AllOf") =>
       Map("test_data_classes" -> classGenerator(ref, t)(table))
-    case t: Composite  =>
-      Map("test_data_classes" -> compositeGenerator(ref, t)(table))
+    case t: Composite =>
+      Map("test_data_classes" -> classGenerator(ref, t)(table))
 
     case _ => empty
   }
@@ -38,46 +40,29 @@ trait DataGeneratorsStep extends EnrichmentStep[Type] {
   private def containerGenerator(k: Reference, v: Type)(table: DenotationTable): Map[String, Any] = {
     Map(
       GENERATOR_NAME -> generatorNameForType(v, table),
-      "creator_method" -> append(prepend("create", typeNameDenotation(table, k)), generatorsSuffix),
-      "generator" -> append(typeNameDenotation(table, k), generatorsSuffix)
+      "creator_method" -> prepend("create", generator(k, table)),
+      "generator" -> generator(k, table)
     )
   }
 
-  private def classGenerator(k: Reference, v: TypeDef)(table: DenotationTable): Map[String, Any] = {
-    Map(
-      GENERATOR_NAME -> generatorNameForType(v, table),
-      "creator_method" -> append(prepend("create", typeNameDenotation(table, k)), generatorsSuffix),
-      "generator" -> append(typeNameDenotation(table, k), generatorsSuffix),
-      "class_name" -> typeNameDenotation(table, k),
-      "fields" -> typeFields(table, k).map { f =>
-        Map(
-          "name" -> escape(f.name.simple),
-          "generator" -> generatorNameForType(f.tpe, table)
-        )
-      }
-    )
-  }
+  private def classGenerator(k: Reference, v: Type)(table: DenotationTable): Map[String, Any] =
+    containerGenerator(k, v)(table) ++
+      Map(
+        "class_name" -> typeNameDenotation(table, k),
+        "fields" -> typeFields(table, k).map { f =>
+          Map(
+            "name" -> escape(f.name.simple),
+            "generator" -> generatorNameForType(f.tpe, table)
+          )
+        }
+      )
 
-  private def compositeGenerator(k: Reference, v: Composite)(table: DenotationTable): Map[String, Any] = {
-    Map(
-      GENERATOR_NAME -> generatorNameForType(v, table),
-      "creator_method" -> append(prepend("create", typeNameDenotation(table, k)), generatorsSuffix),
-      "generator" -> append(typeNameDenotation(table, k), generatorsSuffix),
-      "class_name" -> typeNameDenotation(table, k),
-      "fields" -> typeFields(table, k).map { f =>
-        Map(
-          "name" -> escape(f.name.simple),
-          "generator" -> generatorNameForType(f.tpe, table)
-        )
-      }
-    )
-  }
   private val generatorNameForType: (Type, DenotationTable) => String = {
     case (s: PrimitiveType, table) => primitiveType(s, table)
     case (c: Container, table) => containerType(c, table)
-    case (TypeRef(r), table) => append(typeNameDenotation(table, r), generatorsSuffix)
-    case (d: TypeDef, table) => append(typeNameDenotation(table, d.name), generatorsSuffix)
-    case (c: Composite, table) => append(typeNameDenotation(table, c.name), generatorsSuffix)
+    case (TypeRef(r), table) => generator(r, table)
+    case (d: TypeDef, table) => generator(d.name, table)
+    case (c: Composite, table) => generator(c.name, table)
     case o =>
       throw new Exception("Unexpected reuqest for generator name for: " + o.toString)
   }
@@ -94,5 +79,8 @@ trait DataGeneratorsStep extends EnrichmentStep[Type] {
 
   private def primitiveType(tpe: Type, t: DenotationTable) =
     s"arbitrary[${typeNameDenotation(t, tpe.name)}]"
+
+  private def generator(r: Reference, table: DenotationTable): String =
+    append(typeNameDenotation(table, r), generatorsSuffix)
 
 }
