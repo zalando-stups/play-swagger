599b313b46afe56c3aad0fe3f0c5c6516ea63446
Slava Schmidt <slavaschmidt@gmx.de>
2016-02-18 19:59:39 +0100

    chore: Updat jackson-json-reference, place it in Zalando repo for simplicity

--- compiler/src/test/scala/de/zalando/swagger/StrictParseExamplesTest.scala
+++ compiler/src/test/scala/de/zalando/swagger/StrictParseExamplesTest.scala
@@ -11,11 +11,8 @@ class StrictParseExamplesTest extends FunSpec with MustMatchers {
   val fixtures = new File("compiler/src/test/resources/examples").listFiles ++
     new File("compiler/src/test/resources/schema_examples").listFiles
 
-  // TODO enable these tests after jackson-json-reference is fixed
-  val disabledTests = Seq("body_to_ref_recurse.api.yaml")
-
   describe("Strict Swagger Parser") {
-    fixtures.filter(f => f.getName.endsWith(".yaml") && !disabledTests.contains(f.getName)).foreach { file =>
+    fixtures.filter(_.getName.endsWith(".yaml")).foreach { file =>
       it(s"should parse the yaml swagger file ${file.getName} as specification") {
         val result = StrictYamlParser.parse(file)
         result._1 mustBe a [URI]
