0c32540572b10abc056f5cbffc9d6897051bbeef
Slava Schmidt <slavaschmidt@gmx.de>
2016-03-07 17:03:33 +0100

    #3, feature: Security transformation needed for Extractors

--- compiler/src/main/scala/de/zalando/apifirst/generators/SecurityStep.scala
+++ compiler/src/main/scala/de/zalando/apifirst/generators/SecurityStep.scala
@@ -1,11 +1,8 @@
 package de.zalando.apifirst.generators
 
 import de.zalando.apifirst.Application.StrictModel
-import de.zalando.apifirst.generators.DenotationNames._
-import de.zalando.apifirst.{Security, ParameterPlace, ScalaName}
-import de.zalando.play.controllers.WriterFactories
-
-import scala.ref.WeakReference
+import de.zalando.apifirst.Security._
+import de.zalando.apifirst.{ParameterPlace, ScalaName, Security}
 
 /**
   * @author  slasch
@@ -14,18 +11,45 @@ import scala.ref.WeakReference
 trait SecurityStep extends EnrichmentStep[StrictModel] with SecurityCommons {
 
   override def steps = security +: super.steps
-  
+
   /**
     * Puts security related information into the denotation table
     *
     * @return
     */
   protected val security: SingleStep = spec => table => {
-    val extractors: Iterable[Map[String, String]] = spec._2.securityDefinitionsTable.map { case (name, definition) =>
-      Map("name" -> securityCheck(name))
+    val extractors: Iterable[Map[String, Any]] = spec._2.securityDefinitionsTable.map { case (name, definition) =>
+      Map(
+        "name" -> securityCheck(name),
+        "type" -> securityType(definition),
+        "security_params" -> securityParams(definition),
+        "user_params" -> userParams(definition)
+      )
     }
     if (extractors.isEmpty) Map.empty
     else Map("security_extractors" -> Map("extractors" -> extractors))
+  }
+
+  private def securityType(definition: Definition): String = definition match {
+    case b: Basic => "basicAuth"
+    case ApiKey(_, _, in) if in == ParameterPlace.HEADER => "headerApiKey"
+    case ApiKey(_, _, in) if in == ParameterPlace.QUERY => "queryApiKey"
+    case o: OAuth2Implicit => "oAuth"
+    case o: OAuth2Password => "oAuth"
+    case o: OAuth2Application => "oAuth"
+    case o: OAuth2AccessCode => "oAuth"
+  }
+
+  private def securityParams(definition: Definition): Seq[Map[String,String]] = definition match {
+    case b: Basic => Nil
+    case ApiKey(_, name, _) => Seq(Map("name" -> ("\"" + name + "\"")))
+    case _ => Nil // FIXME
+  }
+
+  private def userParams(definition: Definition): Seq[Map[String,String]] = definition match {
+    case b: Basic => Seq(Map("name" -> "username"), Map("name" -> "password"))
+    case ApiKey(_, name, _) => Seq(Map("name" -> "apiKey"))
+    case _ => Nil // FIXME
   }
 }
 
